37d6df933a535add2b046e2260aec9d7a91f4c62
AbdallahDevMassri <108286230+AbdallahDevMassri@users.noreply.github.com>
2023-05-28 00:52:06 +0300

    starting the project with the basic view

---
+++ app/src/main/java/com/example/bayannails/Pages/Admin_Activity.java
@@ -0,0 +1,14 @@
+package com.example.bayannails.Pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
---
+++ app/src/main/java/com/example/bayannails/Pages/SignUp_activity.java
@@ -0,0 +1,14 @@
+package com.example.bayannails.Pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
